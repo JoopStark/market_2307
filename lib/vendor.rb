ea472b8fad4b3d148cda3c08c7069dd1e5402153
Joop Stark <stark.joop@gmail.com>
2023-08-08 10:02:50 -0600

    vendor feat added, and market class complete

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -17,4 +17,8 @@ class Vendor
   def stock (item, count)
     @inventory[item] += count
   end
+
+  def potential_revenue
+    @inventory.sum { |item, quantity| item.price * quantity }
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,3 @@
 require "./lib/item"
 require "./lib/vendor"
-# require "./lib/market"
+require "./lib/market"
